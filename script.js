533810a01f60b26e2130eefca59138309aa3427e
aqles <70847820+aqles@users.noreply.github.com>
2024-08-28 13:48:03 +0700

    script.js
    major

---
+++ script.js
@@ -0,0 +1,15 @@
+function startGame(game) {
+    const gameArea = document.getElementById('game-area');
+    gameArea.innerHTML = '';
+    if (game === 'snake') {
+        loadScript('games/snake.js');
+    } else if (game === 'tetris') {
+        loadScript('games/tetris.js');
+    }
+}
+
+function loadScript(src) {
+    const script = document.createElement('script');
+    script.src = src;
+    document.body.appendChild(script);
+}
